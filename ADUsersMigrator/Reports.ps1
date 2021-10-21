c86cb73c444c95eb9d894f46525e8ba77fac6d72
Federico Canton <fc@bcc.bz>
2021-10-21 11:37:43 -0700

    add ps1

---
+++ ADUsersMigrator/Reports.ps1
@@ -0,0 +1,14 @@
+Get-ADUser -Filter {enabled -eq $true} -properties * | ? {$_.mail -like "*source.org"} | `
+    select Name, GivenName, SamAccountName, DisplayName, DistinguishedName, UserPrincipalName, `
+    Surname, OtherName, Initials, City, Company, Office, Department, Description, Title, EmailAddress | `
+    Export-Csv -Path C:\temp\users.txt -Delimiter "`t" -NoTypeInformation
+
+$users = Get-ADUser -Filter {enabled -eq $true} -Properties name, mail | ? {$_.mail -like "*source.org"}
+foreach ($user in $users){
+    $groupsdns = get-aduser $user.DistinguishedName -Properties MemberOf | select -ExpandProperty MemberOf
+    $groups = @()
+    foreach ($group in $groupsdns){ $groups += Get-ADGroup $group }
+    foreach ($g in $groups){ 
+        [string]::Format("{0}`t{1}`t{2}`t{3}`t{4}",$user, $g.Name, $g.DistinguishedName, $g.GroupCategory, $g.GroupScope) | `
+        Out-File c:\temp\groups.txt -Append}
+}
